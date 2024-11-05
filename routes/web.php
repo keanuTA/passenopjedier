372c81604acdebec6b341f9af204edf387696d3a
keanuTA <keanutjin95@hotmail.com>
2024-11-05 21:58:46 +0100

    Oppas profiel toegevoegd met route controller en intertia ipv views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SitterProfileController;
 use App\Http\Controllers\PetProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('sitter-profiles', \App\Http\Controllers\SitterProfileController::class);
 });
 
 Route::middleware(['auth'])->group(function () {
