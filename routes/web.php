2a663a3e7ca83c3586089d080796224562c00249
keanuTA <keanutjin95@hotmail.com>
2024-11-06 21:55:14 +0100

    Dashboard links, responsivenes en iconen toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,17 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+// Groep authenticatie routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    // User profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('sitter-profiles', \App\Http\Controllers\SitterProfileController::class);
-});
-
-Route::middleware(['auth'])->group(function () {
+    
+    // Sitter profiles routes
+    Route::resource('sitter-profiles', SitterProfileController::class);
+    
+    // Pet profiles routes
     Route::resource('pet-profiles', PetProfileController::class);
 });
 
